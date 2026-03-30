f579ed42ba62cb49fc4a6d9262c1597386a85b5d
UnknonPerson <tanish90065@gmail.com>
2026-03-30 22:10:15 +0530

    Basic Stuf Done

---
+++ frontend/src/components/Other/AllTask.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const AllTask = () => {
+  return (
+    <div className='bg-[1c1c1c] p-5 rounded mt-5 h-40 overflow-auto'>
+      <div className='bg-red-400 mb-2 py-2 px-4 flex justify-between rounded-2xl'>
+        <h2>Tanish Kumar</h2>
+        <h3>Make a UI Desigine</h3>
+        <h5>Status</h5>
+      </div>
+    </div>
+  )
+}
+
+export default AllTask
